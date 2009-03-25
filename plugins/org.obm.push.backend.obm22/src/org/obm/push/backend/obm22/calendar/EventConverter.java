0aa13e6b524730660107e870c80820d83cf315f3
tcataldo <tcataldo@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2009-03-25 16:26:10 +0000

    2009-03-25  Thomas Cataldo  <thomas.cataldo@aliasource.fr>
    	OBM -> iPhone calendar works :-)
    	
    	* plugins/org.obm.push.backend.obm22/src/org/obm/push/backend/obm22
    	/calendar/CalendarExporter.java: (CalendarExporter.getClient),
    	(CalendarExporter), (CalendarExporter.validateOBMConnection),
    	(CalendarExporter.getHierarchyChanges),
    	(CalendarExporter.getContentChanges),
    	(CalendarExporter.parseCalendarId),
    	(CalendarExporter.addCalendarChange):
    	* plugins/org.obm.push.backend.obm22/src/org/obm/push/backend/obm22
    	/calendar/EventConverter.java: (EventConverter),
    	(EventConverter.convertEvent):
    	* plugins/org.obm.push.tests/.classpath:
    	* plugins/org.obm.push.tests/.project:
    	* plugins/org.obm.push.tests/.settings/org.eclipse.jdt.core.prefs:
    	* plugins/org.obm.push.tests/META-INF/MANIFEST.MF:
    	* plugins/org.obm.push.tests/build.properties:
    	* plugins/org.obm.push.tests/src/org/obm/push/tests/TZDecoderTest.j
    	ava: (TZDecoderTest), (TZDecoderTest.testDecode):
    	* plugins/org.obm.push/META-INF/MANIFEST.MF:
    	* plugins/org.obm.push/push.product:
    	* plugins/org.obm.push/src/org/obm/push/data/CalendarEncoder.java:
    	(CalendarEncoder), (CalendarEncoder.CalendarEncoder),
    	(CalendarEncoder.encode), (CalendarEncoder.e):
    	* plugins/org.obm.push/src/org/obm/push/data/Decoder.java:
    	(Decoder.parseTimeZone):
    	* plugins/org.obm.push/src/org/obm/push/data/TZDecoder.java:
    	(TZDecoder), (TZDecoder.decode):
    	* plugins/org.obm.push/src/org/obm/push/data/calendarenum/CalendarM
    	eetingStatus.java:
    	* plugins/org.obm.sync.client/META-INF/MANIFEST.MF:

--- plugins/org.obm.push.backend.obm22/src/org/obm/push/backend/obm22/calendar/EventConverter.java
+++ plugins/org.obm.push.backend.obm22/src/org/obm/push/backend/obm22/calendar/EventConverter.java
@@ -1,11 +1,22 @@
 package org.obm.push.backend.obm22.calendar;
 
+import java.util.Calendar;
 import java.util.TimeZone;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.obm.push.backend.MSEvent;
 import org.obm.sync.calendar.Event;
 
+/**
+ * Convert events between OBM-Sync object model & Microsoft object model
+ * 
+ * @author tom
+ * 
+ */
 public class EventConverter {
+	
+	private static final Log logger = LogFactory.getLog(EventConverter.class);
 
 	public MSEvent convertEvent(Event e) {
 		MSEvent cal = new MSEvent();
@@ -14,8 +25,14 @@ public class EventConverter {
 		cal.setLocation(e.getLocation());
 		cal.setTimeZone(TimeZone.getTimeZone("Europe/Paris"));
 		cal.setStartTime(e.getDate());
-		
-		
+
+		Calendar c = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		c.setTimeInMillis(e.getDate().getTime());
+		c.add(Calendar.SECOND, e.getDuration());
+		cal.setEndTime(c.getTime());
+
+		logger.info("start: " + cal.getStartTime() + " end: "
+				+ cal.getEndTime());
 		return cal;
 	}
 
--- plugins/org.obm.push/src/org/obm/push/data/CalendarEncoder.java
+++ plugins/org.obm.push/src/org/obm/push/data/CalendarEncoder.java
@@ -1,14 +1,61 @@
 package org.obm.push.data;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 import org.obm.push.backend.IApplicationData;
+import org.obm.push.backend.MSEvent;
+import org.obm.push.data.calendarenum.CalendarBusyStatus;
+import org.obm.push.data.calendarenum.CalendarMeetingStatus;
+import org.obm.push.utils.DOMUtils;
 import org.w3c.dom.Element;
 
 public class CalendarEncoder implements IDataEncoder {
 
-	@Override
-	public void encode(Element parent, IApplicationData data) {
-		// TODO Auto-generated method stub
+	private SimpleDateFormat sdf;
 
+	public CalendarEncoder() {
+		sdf = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+	}
+	
+	// <TimeZone>xP///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoAAAAFAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAFAAIAAAAAAAAAxP///w==</TimeZone>
+	// <AllDayEvent>0</AllDayEvent>
+	// <BusyStatus>2</BusyStatus>
+	// <DTStamp>20010101T000000Z</DTStamp>
+	// <EndTime>20010101T000000Z</EndTime>
+	// <Sensitivity>0</Sensitivity>
+	// <StartTime>20010101T000000Z</StartTime>
+	// <UID>74455CE0E49D486DBDBC7CB224C5212D00000000000000000000000000000000</UID>
+	// <MeetingStatus>0</MeetingStatus>
+
+	@Override
+	public void encode(Element p, IApplicationData data) {
+
+		// TODO Auto-generated method stub
+		MSEvent ev = (MSEvent) data;
+
+		Element tz = DOMUtils.createElement(p, "Calendar:TimeZone");
+		// taken from exchange 2k7 : eastern greenland, gmt+0, no dst
+		tz
+				.setTextContent("xP///1IAbwBtAGEAbgBjAGUAIABTAHQAYQBuAGQAYQByAGQAIABUAGkAbQBlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoAAAAFAAMAAAAAAAAAAAAAAFIAbwBtAGEAbgBjAGUAIABEAGEAeQBsAGkAZwBoAHQAIABUAGkAbQBlAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMAAAAFAAIAAAAAAAAAxP///w==");
+		e(p, "Calendar:AllDayEvent", "0");
+		e(p, "Calendar:BusyStatus", CalendarBusyStatus.BUSY.asIntString());
+
+		e(p, "Calendar:StartTime", sdf.format(ev.getStartTime()));
+		e(p, "Calendar:EndTime", sdf.format(ev.getEndTime()));
+		e(p, "Calendar:UID", ev.getUID());
+		e(p, "Calendar:Subject", ev.getSubject());
+		e(p, "Calendar:Location", ev.getLocation());
+		e(p, "Calendar:MeetingStatus", CalendarMeetingStatus.IS_NOT_IN_MEETING
+				.asIntString());
+
+	}
+
+	private void e(Element p, String name, String val) {
+		if (val != null && val.length() > 0) {
+			DOMUtils.createElementAndText(p, name, val);
+		}
 	}
 
 }
