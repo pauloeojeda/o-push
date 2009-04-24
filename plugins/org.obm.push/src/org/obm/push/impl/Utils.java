e7cd6d6c1419e2e29ebb69b3e49f2294cdd30d05
tcataldo <tcataldo@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2009-04-24 14:31:36 +0000

    add storage APIs

--- plugins/org.obm.push/src/org/obm/push/impl/Utils.java
+++ plugins/org.obm.push/src/org/obm/push/impl/Utils.java
@@ -3,7 +3,7 @@ package org.obm.push.impl;
 public class Utils {
 
 	public static String getFolderId(String devId, String dataClass) {
-		return devId + "/" + dataClass;
+		return devId + "\\" + dataClass;
 	}
 
 }
