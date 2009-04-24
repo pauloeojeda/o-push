de8667d84cfab01598ae6d8141bcc4a16cb67877
tcataldo <tcataldo@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2009-04-24 14:31:36 +0000

    add storage APIs

---
+++ trunk/plugins/org.obm.push/src/org/obm/push/store/ISyncStorage.java
@@ -0,0 +1,21 @@
+package org.obm.push.store;
+
+import org.obm.push.state.SyncState;
+
+public interface ISyncStorage {
+
+	void updateState(String devId, SyncState state);
+	
+	SyncState findStateForDevice(String devId);
+	
+	SyncState findStateForKey(String syncKey);
+	
+	boolean initDevice(String loginAtDomain, String deviceId, String deviceType);
+	
+	void storeMapping(String deviceId, String clientId, String serverId);
+	
+	String getServerId(String deviceId, String clientId);
+
+	String getClientId(String deviceId, String serverId);
+
+}
