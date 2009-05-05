84d84716bb171cd94e8ec1b620cb845d2d10259a
tcataldo <tcataldo@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2009-05-05 14:30:08 +0000

    add delete cascade to schema

--- sql/2.2/create_opush_2.2.sql
+++ sql/2.2/create_opush_2.2.sql
@@ -5,18 +5,18 @@ DROP TABLE IF EXISTS device;
 CREATE TABLE device (
        id 		SERIAL PRIMARY KEY,
        identifier 	VARCHAR(255) NOT NULL,
-       owner		INTEGER REFERENCES userobm(userobm_id),
+       owner		INTEGER REFERENCES userobm(userobm_id) ON DELETE CASCADE,
        type		VARCHAR(64) NOT NULL
 );
 
 CREATE TABLE id_mapping (
-       device_id	INTEGER NOT NULL REFERENCES device(id),
+       device_id	INTEGER NOT NULL REFERENCES device(id) ON DELETE CASCADE,
        client_id	VARCHAR(255) NOT NULL,
        server_id	VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE sync_state (
        sync_key		VARCHAR(64) UNIQUE NOT NULL,
-       device_id	INTEGER NOT NULL REFERENCES device(id),
+       device_id	INTEGER NOT NULL REFERENCES device(id) ON DELETE CASCADE,
        last_sync	TIMESTAMP NOT NULL
 );
