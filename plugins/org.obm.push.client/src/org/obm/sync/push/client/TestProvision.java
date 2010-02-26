63246bcd51f2392a9e6c9bfb5754a571aa62280f
Adrien.Poupard <Adrien.Poupard@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2010-02-26 09:52:17 +0000

    iimproving test

--- plugins/org.obm.push.client/src/org/obm/sync/push/client/TestProvision.java
+++ plugins/org.obm.push.client/src/org/obm/sync/push/client/TestProvision.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 
 import org.obm.push.utils.DOMUtils;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class TestProvision extends AbstractPushTest {
 
@@ -19,9 +20,18 @@ public class TestProvision extends AbstractPushTest {
 	public void testSettingsProvisionProtocol25() throws Exception {
 		optionsQuery();
 
-		InputStream in = loadDataFile("ProvisionRequestProtocol2.5.xml");
+		InputStream in = loadDataFile("ProvisionRequest1Protocol2.5.xml");
 		Document doc = DOMUtils.parse(in);
 		Document ret = postXml("Provision", doc, "Provision", "0", "2.5");
 		assertNotNull(ret);
+		
+		String policyKey = DOMUtils.getElementText(ret.getDocumentElement(), "PolicyKey");
+
+		in = loadDataFile("ProvisionRequest2Protocol2.5.xml");
+		doc = DOMUtils.parse(in);
+		Element elemPolicy = DOMUtils.getUniqueElement(doc.getDocumentElement(), "PolicyKey");
+		elemPolicy.setTextContent(policyKey);
+		ret = postXml("Provision", doc, "Provision", "0", "2.5");
+		
 	}
 }
