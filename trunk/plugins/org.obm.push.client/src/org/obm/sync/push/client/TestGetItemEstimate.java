f5f190d6c968677b2bf86256e782255d8e32943d
Adrien.Poupard <Adrien.Poupard@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2010-03-02 14:01:13 +0000

    Add test

--- trunk/plugins/org.obm.push.client/src/org/obm/sync/push/client/TestGetItemEstimate.java
+++ trunk/plugins/org.obm.push.client/src/org/obm/sync/push/client/TestGetItemEstimate.java
@@ -13,5 +13,16 @@ public class TestGetItemEstimate extends AbstractPushTest {
 		Document ret = postXml("ItemEstimate", doc, "GetItemEstimate");
 		assertNotNull(ret);
 	}
-
+	
+	public void testGetItemEstimate2() throws Exception {
+		InputStream in = loadDataFile("GetItemEstimateRequestError1.xml");
+		Document doc = DOMUtils.parse(in);
+		Document ret = postXml("ItemEstimate", doc, "GetItemEstimate");
+		assertNotNull(ret);
+		
+		in = loadDataFile("SyncRequestError1.xml");
+		doc = DOMUtils.parse(in);
+		ret = postXml("AirSync", doc, "Sync");
+		assertNotNull(ret);
+	}
 }
