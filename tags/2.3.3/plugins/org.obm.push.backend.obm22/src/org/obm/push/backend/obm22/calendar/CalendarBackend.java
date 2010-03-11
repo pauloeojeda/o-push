dc2d3e94ac5c1c04e72c0e1b7290b960412e5b9a
tcataldo <tcataldo@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2010-03-11 13:24:41 +0000

    Create tag 2.3.3

---
+++ tags/2.3.3/plugins/org.obm.push.backend.obm22/src/org/obm/push/backend/obm22/calendar/CalendarBackend.java
@@ -0,0 +1,241 @@
+package org.obm.push.backend.obm22.calendar;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.obm.push.backend.BackendSession;
+import org.obm.push.backend.DataDelta;
+import org.obm.push.backend.FolderType;
+import org.obm.push.backend.ItemChange;
+import org.obm.push.backend.MSAttendee;
+import org.obm.push.backend.MSEvent;
+import org.obm.push.backend.obm22.impl.ObmSyncBackend;
+import org.obm.push.data.calendarenum.AttendeeStatus;
+import org.obm.push.data.calendarenum.AttendeeType;
+import org.obm.push.exception.ActiveSyncException;
+import org.obm.push.store.ISyncStorage;
+import org.obm.sync.auth.AccessToken;
+import org.obm.sync.calendar.CalendarInfo;
+import org.obm.sync.calendar.Event;
+import org.obm.sync.client.calendar.CalendarClient;
+import org.obm.sync.items.EventChanges;
+
+public class CalendarBackend extends ObmSyncBackend {
+
+	public CalendarBackend(ISyncStorage storage) {
+		super(storage);
+	}
+
+	private String getDefaultCalendarName(BackendSession bs) {
+		return "obm:\\\\" + bs.getLoginAtDomain() + "\\calendar\\"
+				+ bs.getLoginAtDomain();
+	}
+
+	public List<ItemChange> getHierarchyChanges(BackendSession bs) {
+		List<ItemChange> ret = new LinkedList<ItemChange>();
+
+		if (!bs.checkHint("hint.multipleCalendars", false)) {
+			ItemChange ic = new ItemChange();
+			String col = getDefaultCalendarName(bs);
+			String serverId;
+			try {
+				serverId = getServerIdFor(bs.getDevId(), col, null);
+			} catch (ActiveSyncException e) {
+				serverId = createCollectionMapping(bs.getDevId(), col);
+				ic.setIsNew(true);
+			}
+			ic.setServerId(serverId);
+			ic.setParentId("0");
+			ic.setDisplayName(bs.getLoginAtDomain()+" calendar");
+			ic.setItemType(FolderType.DEFAULT_CALENDAR_FOLDER);
+			ret.add(ic);
+			return ret;
+		}
+
+		CalendarClient cc = getCalendarClient(bs);
+		AccessToken token = cc.login(bs.getLoginAtDomain(), bs.getPassword(),
+				"o-push");
+		try {
+			CalendarInfo[] cals = cc.listCalendars(token);
+
+			int idx = bs.getLoginAtDomain().indexOf("@");
+			String domain = bs.getLoginAtDomain().substring(idx);
+
+			for (CalendarInfo ci : cals) {
+				ItemChange ic = new ItemChange();
+				String col = "obm:\\\\" + bs.getLoginAtDomain()
+						+ "\\calendar\\" + ci.getUid() + domain;
+				ic.setServerId(getServerIdFor(bs.getDevId(), col, null));
+				ic.setParentId("0");
+				ic.setDisplayName(ci.getMail() + " calendar");
+				if (bs.getLoginAtDomain().equalsIgnoreCase(ci.getMail())) {
+					ic.setItemType(FolderType.DEFAULT_CALENDAR_FOLDER);
+				} else {
+					ic.setItemType(FolderType.USER_CREATED_CALENDAR_FOLDER);
+				}
+				ret.add(ic);
+			}
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+		cc.logout(token);
+
+		return ret;
+	}
+
+	public DataDelta getContentChanges(BackendSession bs, String collection) {
+		List<ItemChange> addUpd = new LinkedList<ItemChange>();
+		List<ItemChange> deletions = new LinkedList<ItemChange>();
+
+		Date ls = bs.getState().getLastSync();
+		CalendarClient cc = getCalendarClient(bs);
+		AccessToken token = cc.login(bs.getLoginAtDomain(), bs.getPassword(),
+				"o-push");
+		String calendar = parseCalendarId(collection);
+		try {
+			EventChanges changes = cc.getSync(token, calendar, ls);
+			Event[] evs = changes.getUpdated();
+			for (Event e : evs) {
+				if(e.getRecurrenceId() == null){
+					ItemChange change = addCalendarChange(bs.getDevId(),
+						collection, e);
+					addUpd.add(change);
+				}
+			}
+			for (String del : changes.getRemoved()) {
+				deletions.add(getDeletion(bs, collection, del));
+			}
+			
+			bs.addUpdatedSyncDate(getCollectionIdFor(bs.getDevId(), collection), changes.getLastSync());
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		}
+
+		cc.logout(token);
+		logger.info("getContentChanges(" + calendar + ", " + collection
+				+ ", lastSync: " + ls + ") => " + addUpd.size() + " entries.");
+		return new DataDelta(addUpd, deletions);
+	}
+
+	private String parseCalendarId(String collectionId) {
+		// parse obm:\\thomas@zz.com\calendar\sylvaing@zz.com
+		logger.info("collectionId: " + collectionId);
+		int slash = collectionId.lastIndexOf("\\");
+		int at = collectionId.lastIndexOf("@");
+
+		return collectionId.substring(slash + 1, at);
+	}
+
+	private ItemChange addCalendarChange(String deviceId, String collection,
+			Event e) throws ActiveSyncException {
+		ItemChange ic = new ItemChange();
+		ic.setServerId(getServerIdFor(deviceId, collection, e.getUid()));
+		MSEvent cal = new EventConverter().convertEvent(e);
+		ic.setData(cal);
+		return ic;
+	}
+
+	public String createOrUpdate(BackendSession bs, String collectionId,
+			String serverId, String clientId, MSEvent data) throws ActiveSyncException {
+		logger.info("createOrUpdate(" + bs.getLoginAtDomain() + ", "
+				+ collectionId + ", " + serverId + ", " + clientId + ", "
+				+ data.getSubject() + ")");
+		CalendarClient cc = getCalendarClient(bs);
+		AccessToken token = cc.login(bs.getLoginAtDomain(), bs.getPassword(),
+				"o-push");
+		String id = null;
+		if (serverId != null) {
+			id = serverId;
+		}
+
+		if (data.getAttendees() == null || data.getAttendees().isEmpty()) {
+			String email = bs.getLoginAtDomain();
+			try {
+				email = cc.getUserEmail(token);
+			} catch (Exception e) {
+				logger.error("Error finding email: " + e.getMessage(), e);
+			}
+			List<MSAttendee> msal = new ArrayList<MSAttendee>(1);
+			MSAttendee at = new MSAttendee();
+			at.setEmail(email);
+			at.setAttendeeStatus(AttendeeStatus.ACCEPT);
+			at.setAttendeeType(AttendeeType.REQUIRED);
+			msal.add(at);
+			data.setAttendees(msal);
+		}
+		Event event = new EventConverter().convertEvent(data);
+		if (id != null) {
+			int idx = id.lastIndexOf(":");
+			id = id.substring(idx + 1);
+			try {
+				event.setUid(id);
+				cc.modifyEvent(token, parseCalendarId(collectionId), event,
+						true);
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			}
+		} else {
+			try {
+				id = cc
+						.createEvent(token, parseCalendarId(collectionId),
+								event);
+			} catch (Exception e) {
+				logger.error(e.getMessage(), e);
+			}
+		}
+		cc.logout(token);
+
+		return getServerIdFor(bs.getDevId(), collectionId, id);
+	}
+
+	public void delete(BackendSession bs, String collectionId, String serverId) {
+		logger.info("delete serverId " + serverId);
+		if (serverId != null) {
+			int idx = serverId.indexOf(":");
+			if (idx > 0) {
+				String id = serverId.substring(idx + 1);
+				CalendarClient bc = getCalendarClient(bs);
+				AccessToken token = bc.login(bs.getLoginAtDomain(), bs
+						.getPassword(), "o-push");
+				try {
+					bc.removeEvent(token, parseCalendarId(collectionId), id);
+				} catch (Exception e) {
+					logger.error(e.getMessage(), e);
+				}
+				bc.logout(token);
+			}
+		}
+	}
+
+	public String updateUserStatus(BackendSession bs, MSEvent data,
+			AttendeeStatus status) {
+		CalendarClient calCli = getCalendarClient(bs);
+		AccessToken at = calCli.login(bs.getLoginAtDomain(), bs.getPassword(),
+				"o-push");
+		try {
+			for (MSAttendee att : data.getAttendees()) {
+				if (bs.getLoginAtDomain().equalsIgnoreCase(att.getEmail())) {
+					att.setAttendeeStatus(status);
+				}
+			}
+
+			int ar = bs.getLoginAtDomain().lastIndexOf("@");
+			String calendar = bs.getLoginAtDomain().substring(0, ar);
+			logger.info("update user status[" + status.toString()
+					+ "] in calendar " + calendar);
+
+			Event event = new EventConverter().convertEvent(data);
+			event = calCli.modifyEvent(at, calendar, event, true);
+			return getServerIdFor(bs.getDevId(), getDefaultCalendarName(bs) + "",
+					event.getUid());
+
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+		} finally {
+			calCli.logout(at);
+		}
+		return null;
+	}
+}
---
+++ tags/2.3.3/plugins/org.obm.push/src/org/obm/push/data/CalendarDecoder.java
@@ -0,0 +1,261 @@
+package org.obm.push.data;
+
+import java.util.ArrayList;
+
+import org.obm.push.backend.MSAttendee;
+import org.obm.push.backend.MSEvent;
+import org.obm.push.backend.IApplicationData;
+import org.obm.push.backend.Recurrence;
+import org.obm.push.data.calendarenum.AttendeeStatus;
+import org.obm.push.data.calendarenum.AttendeeType;
+import org.obm.push.data.calendarenum.CalendarBusyStatus;
+import org.obm.push.data.calendarenum.CalendarMeetingStatus;
+import org.obm.push.data.calendarenum.CalendarSensitivity;
+import org.obm.push.data.calendarenum.RecurrenceDayOfWeek;
+import org.obm.push.data.calendarenum.RecurrenceType;
+import org.obm.push.utils.DOMUtils;
+import org.w3c.dom.Element;
+
+public class CalendarDecoder extends Decoder implements IDataDecoder {
+
+	@Override
+	public IApplicationData decode(Element syncData) {
+		Element containerNode;
+		MSEvent calendar = new MSEvent();
+
+		// Main attributes
+		calendar.setOrganizerName(parseDOMString(DOMUtils.getUniqueElement(
+				syncData, "OrganizerName")));
+		calendar.setOrganizerEmail(parseDOMString(DOMUtils.getUniqueElement(
+				syncData, "OrganizerEmail")));
+		calendar.setUID(parseDOMString(DOMUtils.getUniqueElement(syncData,
+				"UID")));
+		calendar.setTimeZone(parseDOMTimeZone(DOMUtils.getUniqueElement(
+				syncData, "TimeZone")));
+
+		setEventCalendar(calendar, syncData);
+
+		// Components attributes
+
+		// Attendees
+
+		containerNode = DOMUtils.getUniqueElement(syncData, "Attendees");
+		if (containerNode != null) {
+			ArrayList<MSAttendee> attendees = new ArrayList<MSAttendee>();
+			for (int i = 0, n = containerNode.getChildNodes().getLength(); i < n; i += 1) {
+				Element subnode = (Element) containerNode.getChildNodes().item(
+						i);
+				MSAttendee attendee = new MSAttendee();
+
+				attendee.setEmail(parseDOMString(DOMUtils.getUniqueElement(
+						subnode, "Email")));
+				attendee.setName(parseDOMString(DOMUtils.getUniqueElement(
+						subnode, "Name")));
+
+				switch (parseDOMNoNullInt(DOMUtils.getUniqueElement(syncData,
+						"AttendeeStatus"))) {
+				case 0:
+					attendee.setAttendeeStatus(AttendeeStatus.RESPONSE_UNKNOWN);
+					break;
+				case 2:
+					attendee.setAttendeeStatus(AttendeeStatus.TENTATIVE);
+					break;
+				case 3:
+					attendee.setAttendeeStatus(AttendeeStatus.ACCEPT);
+					break;
+				case 4:
+					attendee.setAttendeeStatus(AttendeeStatus.DECLINE);
+					break;
+				case 5:
+					attendee.setAttendeeStatus(AttendeeStatus.NOT_RESPONDED);
+					break;
+				}
+
+				if (attendee.getAttendeeStatus() != null) {
+					logger.info("parse attendeeStatus: "
+							+ attendee.getAttendeeStatus());
+				}
+
+				switch (parseDOMNoNullInt(DOMUtils.getUniqueElement(syncData,
+						"AttendeeStatus"))) {
+				case 1:
+					attendee.setAttendeeType(AttendeeType.REQUIRED);
+					break;
+				case 2:
+					attendee.setAttendeeType(AttendeeType.OPTIONAL);
+					break;
+				case 3:
+					attendee.setAttendeeType(AttendeeType.RESOURCE);
+					break;
+				}
+
+				if (attendee.getAttendeeType() != null) {
+					logger.info("parse attendeeStatus: "
+							+ attendee.getAttendeeType());
+				}
+			}
+			calendar.setAttendees(attendees);
+		}
+		// Exceptions
+
+		containerNode = DOMUtils.getUniqueElement(syncData, "Exceptions");
+		if (containerNode != null) {
+			ArrayList<MSEvent> exceptions = new ArrayList<MSEvent>();
+			for (int i = 0, n = containerNode.getChildNodes().getLength(); i < n; i += 1) {
+				Element subnode = (Element) containerNode.getChildNodes().item(
+						i);
+				MSEvent exception = new MSEvent();
+
+				setEventCalendar(exception, subnode);
+
+				exception.setDeleted(parseDOMInt2Boolean(DOMUtils
+						.getUniqueElement(subnode, "ExceptionIsDeleted")));
+				exception.setExceptionStartTime(parseDOMDate(DOMUtils
+						.getUniqueElement(subnode, "ExceptionStartTime")));
+
+				exception.setMeetingStatus(getMeetingStatus(subnode));
+
+				exception.setSensitivity(getCalendarSensitivity(subnode));
+				exception.setBusyStatus(getCalendarBusyStatus(subnode));
+				exceptions.add(exception);
+			}
+			calendar.setExceptions(exceptions);
+		}
+
+		// Recurrence
+		containerNode = DOMUtils.getUniqueElement(syncData, "Recurrence");
+		if (containerNode != null) {
+			logger.info("decode recurrence");
+			Recurrence recurrence = new Recurrence();
+
+			recurrence.setUntil(parseDOMDate(DOMUtils.getUniqueElement(
+					containerNode, "RecurrenceUntil")));
+			recurrence.setWeekOfMonth(parseDOMInt(DOMUtils.getUniqueElement(
+					containerNode, "RecurrenceWeekOfMonth")));
+			recurrence.setMonthOfYear(parseDOMInt(DOMUtils.getUniqueElement(
+					containerNode, "RecurrenceMonthOfYear")));
+			recurrence.setDayOfMonth(parseDOMInt(DOMUtils.getUniqueElement(
+					containerNode, "RecurrenceDayOfMonth")));
+			recurrence.setOccurrences(parseDOMInt(DOMUtils.getUniqueElement(
+					containerNode, "RecurrenceOccurrences")));
+			recurrence.setInterval(parseDOMInt(DOMUtils.getUniqueElement(
+					containerNode, "RecurrenceInterval")));
+			Integer i = parseDOMInt(DOMUtils.getUniqueElement(containerNode,
+					"RecurrenceDayOfWeek"));
+			if (i != null) {
+				recurrence.setDayOfWeek(RecurrenceDayOfWeek.fromInt(i));
+			}
+
+			switch (parseDOMNoNullInt(DOMUtils.getUniqueElement(containerNode,
+					"RecurrenceType"))) {
+			case 0:
+				recurrence.setType(RecurrenceType.DAILY);
+				break;
+			case 1:
+				recurrence.setType(RecurrenceType.WEEKLY);
+				break;
+			case 2:
+				recurrence.setType(RecurrenceType.MONTHLY);
+				break;
+			case 3:
+				recurrence.setType(RecurrenceType.MONTHLY_NDAY);
+				break;
+			case 5:
+				recurrence.setType(RecurrenceType.YEARLY);
+				break;
+			case 6:
+				recurrence.setType(RecurrenceType.YEARLY_NDAY);
+				break;
+			}
+
+			if (recurrence.getType() != null) {
+				logger.info("parse type: " + recurrence.getType());
+			}
+
+			calendar.setRecurrence(recurrence);
+		}
+
+		return calendar;
+	}
+
+	void setEventCalendar(MSEvent calendar, Element domSource) {
+		calendar.setLocation(parseDOMString(DOMUtils.getUniqueElement(
+				domSource, "Location")));
+		calendar.setDtStamp(parseDOMDate(DOMUtils.getUniqueElement(domSource,
+				"DTStamp")));
+		calendar.setSubject(parseDOMString(DOMUtils.getUniqueElement(domSource,
+				"Subject")));
+		calendar.setEndTime(parseDOMDate(DOMUtils.getUniqueElement(domSource,
+				"EndTime")));
+		calendar.setStartTime(parseDOMDate(DOMUtils.getUniqueElement(domSource,
+				"StartTime")));
+		calendar.setAllDayEvent(parseDOMInt2Boolean(DOMUtils.getUniqueElement(
+				domSource, "AllDayEvent")));
+		calendar.setReminder(parseDOMInt(DOMUtils.getUniqueElement(domSource,
+				"ReminderMinsBefore")));
+		calendar.setCategories(parseDOMStringCollection(DOMUtils
+				.getUniqueElement(domSource, "Categories"), "Category"));
+
+		calendar.setBusyStatus(getCalendarBusyStatus(domSource));
+		if (calendar.getBusyStatus() != null) {
+			logger.info("parse busyStatus: " + calendar.getBusyStatus());
+		}
+
+		calendar.setSensitivity(getCalendarSensitivity(domSource));
+		if (calendar.getSensitivity() != null) {
+			logger.info("parse sensitivity: " + calendar.getSensitivity());
+		}
+
+		calendar.setMeetingStatus(getMeetingStatus(domSource));
+		if (calendar.getMeetingStatus() != null) {
+			logger.info("parse meetingStatus: " + calendar.getMeetingStatus());
+		}
+	}
+
+	private CalendarBusyStatus getCalendarBusyStatus(Element domSource) {
+		switch (parseDOMNoNullInt(DOMUtils.getUniqueElement(domSource,
+				"BusyStatus"))) {
+		case 0:
+			return CalendarBusyStatus.FREE;
+		case 1:
+			return CalendarBusyStatus.TENTATIVE;
+		case 2:
+			return CalendarBusyStatus.BUSY;
+		case 3:
+			return CalendarBusyStatus.OUT_OF_OFFICE;
+		}
+		return null;
+	}
+
+	private CalendarSensitivity getCalendarSensitivity(Element domSource) {
+		switch (parseDOMNoNullInt(DOMUtils.getUniqueElement(domSource,
+				"Sensitivity"))) {
+		case 0:
+			return CalendarSensitivity.NORMAL;
+		case 1:
+			return CalendarSensitivity.PERSONAL;
+		case 2:
+			return CalendarSensitivity.PRIVATE;
+		case 3:
+			return CalendarSensitivity.CONFIDENTIAL;
+		}
+		return null;
+	}
+
+	private CalendarMeetingStatus getMeetingStatus(Element domSource) {
+		switch (parseDOMNoNullInt(DOMUtils.getUniqueElement(domSource,
+				"MeetingStatus"))) {
+		case 0:
+			return CalendarMeetingStatus.IS_NOT_IN_MEETING;
+		case 1:
+			return CalendarMeetingStatus.IS_IN_MEETING;
+		case 3:
+			return CalendarMeetingStatus.MEETING_RECEIVED;
+		case 5:
+			return CalendarMeetingStatus.MEETING_IS_CANCELED;
+		case 7:
+			return CalendarMeetingStatus.MEETING_IS_CANCELED_AND_RECEIVED;
+		}
+		return null;
+	}
+}
