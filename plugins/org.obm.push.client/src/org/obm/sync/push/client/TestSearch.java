e0ccad6b2bb7b5fdcec5057c0406b1dc68530b33
Adrien.Poupard <Adrien.Poupard@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2010-03-31 13:12:10 +0000

    Test

--- plugins/org.obm.push.client/src/org/obm/sync/push/client/TestSearch.java
+++ plugins/org.obm.push.client/src/org/obm/sync/push/client/TestSearch.java
@@ -4,16 +4,29 @@ import java.io.InputStream;
 
 import org.obm.push.utils.DOMUtils;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class TestSearch extends AbstractPushTest {
 
-	public void testSearch() throws Exception {
+	public void testSearchIphone() throws Exception {
 		optionsQuery();
 
-		InputStream in = loadDataFile("SearchRequest.xml");
+		InputStream in = loadDataFile("SearchRequestIphone.xml");
 		Document doc = DOMUtils.parse(in);
+		Document ret = postXml25("Search", doc, "Search");
+		assertNotNull(ret);
+	}
+	
+	public void testSearchWM65() throws Exception {
+		optionsQuery();
+
+		InputStream in = loadDataFile("SearchRequestWM65.xml");
+		Document doc = DOMUtils.parse(in);
+		Element query = DOMUtils.getUniqueElement(doc.getDocumentElement(), "Query");
+		query.setTextContent("Me");
 		Document ret = postXml("Search", doc, "Search");
 		assertNotNull(ret);
 	}
+
 
 }
