5fe21508c29b5086d9689766a6ff1c38b6072afb
tcataldo <tcataldo@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2010-09-02 14:31:27 +0000

    add developper mode with data in the logs

--- plugins/org.obm.push/src/org/obm/push/impl/DOMDumper.java
+++ plugins/org.obm.push/src/org/obm/push/impl/DOMDumper.java
@@ -1,6 +1,7 @@
 package org.obm.push.impl;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 import javax.xml.transform.TransformerException;
 
@@ -12,6 +13,9 @@ import org.w3c.dom.NodeList;
 
 public class DOMDumper {
 
+	private static boolean withData = new File("/etc/opush/data.in.logs")
+			.exists();
+
 	/**
 	 * Seeing email/cal/contact data is a security issue for some
 	 * administrators. Remove data from a copy of the DOM before printing.
@@ -21,19 +25,20 @@ public class DOMDumper {
 	public static void dumpXml(Log logger, String prefix, Document doc) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		try {
-			logger.info("before clone");
 			Document c = DOMUtils.cloneDOM(doc);
-			logger.info("after clone");
-			NodeList nl = c.getElementsByTagName("ApplicationData");
-			for (int i = 0; i < nl.getLength(); i++) {
-				Node e = nl.item(i);
-				NodeList children = e.getChildNodes();
-				for (int j = 0; j < children.getLength(); j++) {
-					Node child = children.item(j);
-					e.removeChild(child);
-				}
 
-				e.setTextContent("[trimmed_output]");
+			if (!withData) {
+				NodeList nl = c.getElementsByTagName("ApplicationData");
+				for (int i = 0; i < nl.getLength(); i++) {
+					Node e = nl.item(i);
+					NodeList children = e.getChildNodes();
+					for (int j = 0; j < children.getLength(); j++) {
+						Node child = children.item(j);
+						e.removeChild(child);
+					}
+
+					e.setTextContent("[trimmed_output]");
+				}
 			}
 
 			DOMUtils.serialise(c, out, true);
