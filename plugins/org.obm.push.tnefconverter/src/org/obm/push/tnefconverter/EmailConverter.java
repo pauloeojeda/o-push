95ad6dc6d096377664fcf7f2ab0420ed24cbf6c4
Adrien.Poupard <Adrien.Poupard@9c53a268-eeb9-11dd-901b-0b69d2339e2b>
2010-09-23 07:30:19 +0000

    fix null pointer

--- plugins/org.obm.push.tnefconverter/src/org/obm/push/tnefconverter/EmailConverter.java
+++ plugins/org.obm.push.tnefconverter/src/org/obm/push/tnefconverter/EmailConverter.java
@@ -32,8 +32,7 @@ public class EmailConverter {
 
 	private static Log logger = LogFactory.getLog(EmailConverter.class);
 
-	public InputStream convert(InputStream email)
-			throws TNEFConverterException {
+	public InputStream convert(InputStream email) throws TNEFConverterException {
 		try {
 			MimeStreamParser parser = new MimeStreamParser();
 			EmailTnefHandler handler = new EmailTnefHandler();
@@ -41,71 +40,80 @@ public class EmailConverter {
 			parser.parse(email);
 
 			Message message = handler.getTNEFMsg();
-			if (logger.isDebugEnabled()) {
-				logger.debug(message);
-			}
-			if (TNEFExtractorUtils.isScheduleMeetingRequest(message)) {
-				ScheduleMeeting meeting = new ScheduleMeeting(message);
-				ScheduleMeetingEncoder encoder = new ScheduleMeetingEncoder(
-						meeting, handler.getSubject(), handler.getFrom(),
-						handler.getTo(), handler.getCc());
-				String ics = encoder.encodeToIcs();
-				logger.info(ics);
-
-				Header rootHeader = new Header();
-
-				BasicHeader basicHeader = new BasicHeader(rootHeader);
-				basicHeader.setTo(handler.getTo().toArray(new Address[0]));
-				basicHeader.setSubject(handler.getSubject(), Charset
-						.defaultCharset());
-				basicHeader.set("Thread-Topic", handler.getSubject());
-				basicHeader.setFrom(handler.getFrom());
-				if (handler.getCc().size() > 0) {
-					basicHeader.setCc(handler.getCc().toArray(new Address[0]));
+			if (message != null) {
+				if (logger.isDebugEnabled()) {
+					logger.debug(message);
 				}
+				if (TNEFExtractorUtils.isScheduleMeetingRequest(message)) {
+					ScheduleMeeting meeting = new ScheduleMeeting(message);
+					ScheduleMeetingEncoder encoder = new ScheduleMeetingEncoder(
+							meeting, handler.getSubject(), handler.getFrom(),
+							handler.getTo(), handler.getCc());
+					String ics = encoder.encodeToIcs();
+					if (logger.isDebugEnabled()) {
+						logger.debug("ICS from tnef: " + ics);
+					}
 
-				MimeHeader mh = new MimeHeader(rootHeader);
-				mh.set("MIME-Version", "1.0");
-				mh.setMimeType(new MimeType("multipart", "mixed"));
-				LocalMimePart root = new LocalMimePart(mh);
+					Header rootHeader = new Header();
 
-				MimeHeader textMimeHeader = new MimeHeader(new Header());
-				textMimeHeader.set("Content-Type",
-						"text/plain; charset=\"UTF-8\"");
-				textMimeHeader.setContentTransferEncoding("8bit");
-				LocalMimePart textPart = new LocalMimePart(textMimeHeader);
-				textPart.setBody(new CharSequenceSource(meeting.getDescription()));
-				root.addChild(textPart);
+					BasicHeader basicHeader = new BasicHeader(rootHeader);
+					basicHeader.setTo(handler.getTo().toArray(new Address[0]));
+					basicHeader.setSubject(handler.getSubject(), Charset
+							.defaultCharset());
+					basicHeader.set("Thread-Topic", handler.getSubject());
+					basicHeader.setFrom(handler.getFrom());
+					if (handler.getCc().size() > 0) {
+						basicHeader.setCc(handler.getCc().toArray(
+								new Address[0]));
+					}
 
-				MimeHeader requestMimeHeader = new MimeHeader(new Header());
-				String method = "REQUEST";
-				if(PidTagMessageClass.ScheduleMeetingCanceled.equals(meeting.getMethod())){
-					method = "CANCEL";
+					MimeHeader mh = new MimeHeader(rootHeader);
+					mh.set("MIME-Version", "1.0");
+					mh.setMimeType(new MimeType("multipart", "mixed"));
+					LocalMimePart root = new LocalMimePart(mh);
+
+					MimeHeader textMimeHeader = new MimeHeader(new Header());
+					textMimeHeader.set("Content-Type",
+							"text/plain; charset=\"UTF-8\"");
+					textMimeHeader.setContentTransferEncoding("8bit");
+					LocalMimePart textPart = new LocalMimePart(textMimeHeader);
+					textPart.setBody(new CharSequenceSource(meeting
+							.getDescription()));
+					root.addChild(textPart);
+
+					MimeHeader requestMimeHeader = new MimeHeader(new Header());
+					String method = "REQUEST";
+					if (PidTagMessageClass.ScheduleMeetingCanceled
+							.equals(meeting.getMethod())) {
+						method = "CANCEL";
+					}
+					requestMimeHeader.set("Content-Type",
+							"text/calendar; charset=\"UTF-8\"; method="
+									+ method + "; charset=\"UTF-8\"");
+					requestMimeHeader.setContentTransferEncoding("8bit");
+					LocalMimePart requestPart = new LocalMimePart(
+							requestMimeHeader);
+					requestPart.setBody(new CharSequenceSource(ics.replace(
+							"\0", "")
+							+ "\n"));
+					root.addChild(requestPart);
+
+					MimeHeader tnefMimeHeader = new MimeHeader(new Header());
+					tnefMimeHeader.set("Content-Type", "application/ms-tnef");
+					tnefMimeHeader.set("X-MS-Has-Attach", "");
+					tnefMimeHeader.setContentTransferEncoding("base64");
+					tnefMimeHeader.setContentDisposition("attachment");
+					LocalMimePart tnefPart = new LocalMimePart(tnefMimeHeader);
+					byte[] b = FileUtils
+							.streamBytes(handler.getTnefDoc(), true);
+					tnefPart.setBody(new CharSequenceSource(new String(Base64
+							.encode(b))));
+					root.addChild(tnefPart);
+					InputStream in = MimeTreeRenderer.getInstance()
+							.renderMimePart(root);
+					return in;
+
 				}
-				requestMimeHeader
-						.set("Content-Type",
-								"text/calendar; charset=\"UTF-8\"; method="+method+"; charset=\"UTF-8\"");
-				requestMimeHeader.setContentTransferEncoding("8bit");
-				LocalMimePart requestPart = new LocalMimePart(requestMimeHeader);
-				requestPart.setBody(new CharSequenceSource(ics
-						.replace("\0", "")
-						+ "\n"));
-				root.addChild(requestPart);
-
-				MimeHeader tnefMimeHeader = new MimeHeader(new Header());
-				tnefMimeHeader.set("Content-Type", "application/ms-tnef");
-				tnefMimeHeader.set("X-MS-Has-Attach", "");
-				tnefMimeHeader.setContentTransferEncoding("base64");
-				tnefMimeHeader.setContentDisposition("attachment");
-				LocalMimePart tnefPart = new LocalMimePart(tnefMimeHeader);
-				byte[] b = FileUtils.streamBytes( handler.getTnefDoc(), true);
-				tnefPart.setBody(new CharSequenceSource(new String(Base64
-						.encode(b))));
-				root.addChild(tnefPart);
-				InputStream in = MimeTreeRenderer.getInstance().renderMimePart(
-						root);
-				return in;
-
 			}
 
 			return email;
